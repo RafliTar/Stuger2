c35f5143a0aef1f02804c5fcfef8b4ed13fc0d55
Felixlix45 <felixlix45@gmail.com>
2018-12-23 22:26:14 +0700

    Sign In View (Belum backend)

--- app/src/main/java/com/example/felix/stuger/MainActivity.java
+++ app/src/main/java/com/example/felix/stuger/MainActivity.java
@@ -1,13 +1,40 @@
 package com.example.felix.stuger;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button btnSignIn;
+    EditText etLoginEmail, etLoginPassword;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnSignIn = (Button)findViewById(R.id.btnSignIn);
+        etLoginEmail = (EditText)findViewById(R.id.etLoginEmail);
+        etLoginPassword = (EditText)findViewById(R.id.etLoginPassword);
+
+        btnSignIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String email = etLoginEmail.getText().toString();
+                String password = etLoginPassword.getText().toString();
+
+                if(email.equals("admin") && password.equals("admin")){
+                    Intent intent = new Intent(getApplicationContext(), DashboardActivity.class);
+                    startActivity(intent);
+                    finish();
+                }else{
+                    //Validasi email, validasi email di msUser
+                }
+            }
+        });
     }
 }
