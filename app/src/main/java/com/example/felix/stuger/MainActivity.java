51aea5266a488b9701dc5cb4f8158acaedbf2558
Felixlix45 <felixlix45@gmail.com>
2018-12-24 19:58:52 +0700

    Create Sign Up View

--- app/src/main/java/com/example/felix/stuger/MainActivity.java
+++ app/src/main/java/com/example/felix/stuger/MainActivity.java
@@ -3,14 +3,25 @@ package com.example.felix.stuger;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.SpannableString;
+import android.text.style.UnderlineSpan;
+import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import org.w3c.dom.Text;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class MainActivity extends AppCompatActivity {
 
     Button btnSignIn;
     EditText etLoginEmail, etLoginPassword;
+    TextView tvSignUp;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,6 +31,19 @@ public class MainActivity extends AppCompatActivity {
         btnSignIn = (Button)findViewById(R.id.btnSignIn);
         etLoginEmail = (EditText)findViewById(R.id.etLoginEmail);
         etLoginPassword = (EditText)findViewById(R.id.etLoginPassword);
+        tvSignUp = (TextView) findViewById(R.id.tvSignUp);
+
+        SpannableString content = new SpannableString("Sign Up");
+        content.setSpan(new UnderlineSpan(), 0, content.length(), 0);
+        tvSignUp.setText(content);
+
+        tvSignUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), SignUpStepOneActivity.class);
+                startActivity(intent);
+            }
+        });
 
         btnSignIn.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -32,9 +56,26 @@ public class MainActivity extends AppCompatActivity {
                     startActivity(intent);
                     finish();
                 }else{
-                    //Validasi email, validasi email di msUser
+                    if(isEmail(email) && password.length()>0){
+                        Intent intent = new Intent(getApplicationContext(), DashboardActivity.class);
+                        startActivity(intent);
+                        finish();
+                    }else{
+                        Toast.makeText(MainActivity.this, "Check your email & password again", Toast.LENGTH_SHORT).show();
+                    }
                 }
             }
         });
+    }
+
+    public boolean isEmail(String email){
+        String regex = "^[a-zA-Z0-9_!#$%&'*+/=?`{|}~^.-]+@[a-zA-Z0-9.-]+$";
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(email);
+        if(matcher.matches() == true){
+            return true;
+        }else{
+            return false;
+        }
     }
 }
