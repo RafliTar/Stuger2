46dd3a611b0627efa6b9a684f3c1648a8575c983
bryan8288 <bc18988@gmail.com>
2019-01-09 23:23:43 +0700

    Quiz's Display

--- app/src/main/java/com/example/felix/stuger/QuizFragment.java
+++ app/src/main/java/com/example/felix/stuger/QuizFragment.java
@@ -1,5 +1,6 @@
 package com.example.felix.stuger;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
@@ -7,11 +8,34 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 public class QuizFragment extends Fragment {
+    public Button btnIPA;
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_quiz, container, false);
+        View v =  inflater.inflate(R.layout.fragment_quiz, container, false);
+
+        Button btnIPS = (Button) v.findViewById(R.id.buttonIPS);
+        Button btnIPA = (Button) v.findViewById(R.id.buttonIPA);
+        btnIPS.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent IPS = new Intent(getActivity(),IPSActivity.class);
+                startActivity(IPS);
+            }
+        });
+        btnIPA.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent IPA = new Intent(getActivity(),IPAActivity.class);
+                startActivity(IPA);
+            }
+        });
+
+        return  v;
+
     }
 }
+
